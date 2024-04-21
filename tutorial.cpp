e840e6f264854bc0d5441b2cc1ae0e5ee56cc15a
Ben Malpas <benamalpas@gmail.com>
2024-04-21 23:14:24 +0100

    Follow step 1 of the CMake tutorial

---
+++ tutorial.cpp
@@ -0,0 +1,10 @@
+#include "tutorial_config.hpp"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR;
+
+    return 1;
+}
---
+++ tutorial.hpp
@@ -0,0 +1,1 @@
+#include "tutorial_config.hpp"
